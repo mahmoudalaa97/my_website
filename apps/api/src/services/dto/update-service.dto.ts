4289b2192d5a62fd2b672e877e4054453d5f53d0
Mahmoud Alaa <mahmoudadmob@gmail.com>
2026-05-02 02:36:17 +0300

    remvoe the api nextjs from the project

--- apps/api/src/services/dto/update-service.dto.ts
+++
@@ -1,5 +0,0 @@
-import { PartialType } from '@nestjs/mapped-types';
-import { CreateServiceDto } from './create-service.dto';
-
-export class UpdateServiceDto extends PartialType(CreateServiceDto) {}
-
--- apps/api/src/users/dto/reset-password.dto.ts
+++
@@ -1,8 +0,0 @@
-import { IsString, MinLength } from 'class-validator';
-
-export class ResetPasswordDto {
-  @IsString()
-  @MinLength(8, { message: 'Password must be at least 8 characters long' })
-  newPassword: string;
-}
-
